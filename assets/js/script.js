edf5b25282a4f9fb513d45dc82f31a54a1ec8ddd
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-03-30 16:09:07 -0600

    added porject 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
-$(".project-card").hover(
-    function () {
-        $(this).removeClass("blur").css("cursor", "pointer");
-    },
-    function () {
-        $(this).addClass("blur");
-    }
-);
-$(".bio").hover(
-    function () {
-        $(this).removeClass("blur").css("cursor", "pointer");
-    },
-    function () {
-        $(this).addClass("blur");
-    }
-);
+// $(".project-card").hover(
+//     function () {
+//         $(this).removeClass("blur").css("cursor", "pointer");
+//     },
+//     function () {
+//         $(this).addClass("blur");
+//     }
+// );
+// $(".bio").hover(
+//     function () {
+//         $(this).removeClass("blur").css("cursor", "pointer");
+//     },
+//     function () {
+//         $(this).addClass("blur");
+//     }
+// );
 
 $('.project-card').on('click', function() {
     openProject();
