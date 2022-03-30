1fa314b70a012ee611d392d1c8b101e7d3c346b9
Joseph Dugan <Joseph.Michael.Dugan@gmail.com>
2022-03-30 15:45:58 -0600

    copied files from my last protfolio

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(".project-card").hover(
+    function () {
+        $(this).removeClass("blur").css("cursor", "pointer");
+    },
+    function () {
+        $(this).addClass("blur");
+    }
+);
+$(".bio").hover(
+    function () {
+        $(this).removeClass("blur").css("cursor", "pointer");
+    },
+    function () {
+        $(this).addClass("blur");
+    }
+);
+
+$('.project-card').on('click', function() {
+    openProject();
+})
+
+
